1a91007700486c57e14ddaa088be1e9e297f397c
Sola Samuel <oluwasolasamuel4@gmail.com>
2022-03-30 21:08:04 +0100

    setup

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="container container-fluid">
+            <h1>STARLING BANK</h1>
+
+            <button type="button" class="btn btn-primary">CHOOSE ACCOUNT</button>
+        </div>
+    )
+}
+
+export default Home
